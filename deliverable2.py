6f54e6a170e5a34b24440b0e6e1b7227927cedf3
GoooBeavs <klingerg@oregonstate.edu>
2024-06-04 12:50:24 -0700

    Added all functions, some comments

--- deliverable2.py
+++ deliverable2.py
@@ -3,8 +3,18 @@ from ldap3 import Server, Connection, ALL, SUBTREE
 import json
 import requests
 
+#script 1
+def get_courses():
+    return
 
+#script 2
+def get_instructors(year, term, crn):
+    return
+
+
+#script 3
 def get_emails(firstname, lastname, credentialsfile):
+    return
 
 
 #code goes here
